d952d69340c37c793db7b81813435689c791ae0d
Jens <jens.jens@live.dk>
2023-11-12 21:20:04 +0100

    feat: header widget

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,7 @@ import 'package:flame/game.dart';
 // import 'package:flame/text.dart';
 import 'package:flutter/material.dart';
 import 'widgets/sim_visualiser.dart';
-import 'widgets/glass.dart';
+import 'widgets/header.dart';
 // import 'widgets/sim_world.dart';
 // import 'package:flutter/gestures.dart';
 // import '../logger.dart';
@@ -46,53 +46,8 @@ class Home extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(20),
-            child: Glass(
-              height: 100,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Center(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        child: Text(
-                          "UrbanOS",
-                          style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                                color: Theme.of(context).colorScheme.onBackground,
-                                fontFamily: 'Inter',
-                                fontWeight: FontWeight.w300,
-                              ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  MaterialButton(
-                    hoverColor: Theme.of(context).colorScheme.onBackground.withOpacity(0.25),
-                    padding: const EdgeInsets.all(0),
-                    minWidth: 0,
-                    onPressed: () {
-                      if (_themeNotifier.value == 'latte') {
-                        _themeNotifier.value = 'mocha';
-                      } else {
-                        _themeNotifier.value = 'latte';
-                      }
-                    },
-                    shape: CircleBorder(
-                        side: BorderSide(
-                      color: Theme.of(context).colorScheme.onBackground,
-                      width: 2,
-                    )),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Icon(
-                        _themeNotifier.value == 'latte'
-                            ? Icons.dark_mode_outlined
-                            : Icons.light_mode_outlined,
-                        color: Theme.of(context).colorScheme.onBackground,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            child: Header(
+              themeNotifier: _themeNotifier,
             ),
           ),
         ],
