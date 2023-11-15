46fbded94ac36d5271b4769688080ef552f90e62
Jens <jens.jens@live.com>
2023-11-15 13:46:00 +0100

    feat: init setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 import 'themes/themes.dart';
-import '../websocket/simulation_api.dart';
+// import '../websocket/simulation_api.dart';
+import "package:dart_amqp/dart_amqp.dart";
+import "logger.dart";
 
-void main() {
-  SimulationAPI.connect();
+void main() async {
+  // SimulationAPI.connect();
+
+  Client client = Client(
+    settings: ConnectionSettings(
+      host: "localhost",
+      port: 9001,
+    ),
+  );
+  Channel channel = await client.channel();
+  Queue queue = await channel.queue("hello");
+  Consumer consumer = await queue.consume();
+  consumer.listen((AmqpMessage message) {
+    // Get the payload as a string
+    l.i(" [x] Received string: ${message.payloadAsString}");
+
+    // Or unserialize to json
+    l.i(" [x] Received json: ${message.payloadAsJson}");
+
+    // Or just get the raw data as a Uint8List
+    l.i(" [x] Received raw: ${message.payload}");
+
+    // The message object contains helper methods for
+    // replying, ack-ing and rejecting
+    message.reply("world");
+  });
+
   runApp(const MyApp());
 }
 
