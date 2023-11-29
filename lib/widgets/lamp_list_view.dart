3636b95046473d858943014c38821e3ef53c7dde
Pernille <pernillepallesen3@gmail.com>
2023-11-29 15:09:25 +0100

    feat: no scrollbar overlap and scrollbar styling

--- lib/widgets/lamp_list_view.dart
+++ lib/widgets/lamp_list_view.dart
@@ -17,7 +17,7 @@ class LampListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.only(left: 30, right: 15.0),
       child: Column(
         children: [
           Padding(
@@ -28,42 +28,59 @@ class LampListView extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: ShaderMask(
-              shaderCallback: (Rect rect) {
-                return LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.transparent,
-                    theme.colorScheme.surface,
-                    theme.colorScheme.surface,
-                    Colors.transparent
-                  ],
-                  stops: const [
-                    0.0,
-                    0.05,
-                    0.95,
-                    1.0
-                  ], // Adjust the middle stops closer to the ends
-                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-              },
-              blendMode: BlendMode.dstIn,
-              child: ListView.separated(
-                padding: const EdgeInsets.only(
-                    bottom: 30.0, top: 20.0), // Add bottom padding
-                itemCount: lamps.length,
-                itemBuilder: (context, index) {
-                  return LampItem(
-                    lamp: lamps[index],
-                    onClick: () {
-                      onPressed(index);
+            child: ScrollbarTheme(
+              data: ScrollbarThemeData(
+                thumbColor: MaterialStateProperty.all(theme
+                    .colorScheme.onBackground
+                    .withOpacity(0.2)), // Thumb color
+                thickness: MaterialStateProperty.all(
+                    6.0), // Thickness of the scrollbar
+                radius:
+                    const Radius.circular(8), // Radius of the scrollbar thumb
+                // Add any other necessary ScrollbarThemeData properties here
+              ),
+              child: ShaderMask(
+                shaderCallback: (Rect rect) {
+                  return LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.transparent,
+                      theme.colorScheme.surface,
+                      theme.colorScheme.surface,
+                      Colors.transparent
+                    ],
+                    stops: const [
+                      0.0,
+                      0.05,
+                      0.95,
+                      1.0
+                    ], // Adjust the middle stops closer to the ends
+                  ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                },
+                blendMode: BlendMode.dstIn,
+                child: Scrollbar(
+                  child: ListView.separated(
+                    primary: true,
+                    padding: const EdgeInsets.only(
+                        bottom: 30.0,
+                        top: 20.0,
+                        right: 15.0), // Adjusted padding for top and bottom
+                    itemCount: lamps.length,
+                    itemBuilder: (context, index) {
+                      return LampItem(
+                        lamp: lamps[index],
+                        onClick: () {
+                          onPressed(index);
+                        },
+                      );
                     },
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                      height: 8.0); // Maintain separation between items
-                },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                          height: 8.0); // Maintain separation between items
+                    },
+                  ),
+                ),
               ),
             ),
           ),
